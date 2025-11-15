a123a8e136ad78bc55ae4dc2a7aec1370c8d6756
ruiarmsilva-prog <ruiarmsilva@esmonserrate.org>
2025-11-15 14:05:47 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ def gerar_dorsal_a6(nome, processo, escalao):
     # Texto na metade inferior
     bottom_y = qr_size
 
-    font_name = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 100)
-    font_esc = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 100)
+    font_name = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 50)
+    font_esc = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 50)
 
     col_width = A6_WIDTH // 2
     left_center_x = col_width // 2
